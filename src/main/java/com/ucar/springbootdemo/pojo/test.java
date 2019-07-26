1d40db3c39bcaf9aa1d31fa66e74432957e9e9b2
peng.zhang11 <peng.zhang11@ucarinc.com>
2019-07-26 10:18:24 +0800

    test

---
+++ src/main/java/com/ucar/springbootdemo/pojo/test.java
@@ -0,0 +1,10 @@
+package com.ucar.springbootdemo.pojo;
+
+/**
+ * @program: springbootdemo
+ * @description: test
+ * @author: peng.zhang11@ucarinc.com
+ * @create: 2019-07-26 10:15
+ */
+public class test {
+}
