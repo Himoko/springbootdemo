78632a4b2ecee5967552e77e2898ae61b8beaed8
peng.zhang11 <peng.zhang11@ucarinc.com>
2019-10-22 16:29:46 +0800

    更改配置

--- src/main/java/com/ucar/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/ucar/springbootdemo/SpringbootdemoApplication.java
@@ -2,12 +2,25 @@ package com.ucar.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * description: 启动类
+ * @author peng.zhang11@ucarinc.com
+ * @date   2019/10/22 16:21
+ * @params * @Param null:
+ * @return * @return: null
+ */
 @SpringBootApplication
-public class SpringbootdemoApplication {
+public class SpringbootdemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
